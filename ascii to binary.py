48d17e7a18ffd70e2eca1c954b8e058980377be6
Jay Williams <codingJWilliams@users.noreply.github.com>
2017-12-18 19:16:21 +0000

    Fix weird python

--- ascii to binary.py
+++ ascii to binary.py
@@ -3,6 +3,6 @@ word=input("Enter the text you want to be converted ")
 # oof what was that
 out = ""
 for ch in word:
-  out = out + " " + bin(ord(ch))
+  out = out + " 0" + bin(ord(ch))[2:].zfill(8)
 print("Binary: ")
 print(out)
